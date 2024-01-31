f6512c66304fd5665044766743bf5fb38986f2c0
King <98532667+Kingofpig151251@users.noreply.github.com>
2024-01-31 17:08:55 +0800

    First Up

---
+++ robots/BaseTank/BaseTank.java
@@ -0,0 +1,32 @@
+package BaseTank;
+import robocode.*;
+//import java.awt.Color;
+
+// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
+
+abstract public class BaseTank extends AdvancedRobot {
+
+	EnemyBot enemy = new EnemyBot();
+	boolean enemyUpdated = false;
+
+	public void run() {
+		setAdjustGunForRobotTurn((true));
+		setAdjustRadarForGunTurn(true);
+		setAdjustRadarForRobotTurn(true);
+		while (true) {
+			doRadar();
+			doGun();
+			doMove();
+			if (enemyUpdated == true)
+				enemyUpdated = false;
+			execute();
+		}
+	}
+
+	abstract public void doRadar();
+
+	abstract public void doGun();
+
+	abstract public void doMove();
+
+}
